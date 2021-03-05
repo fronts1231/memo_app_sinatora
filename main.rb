f696ddf4e8c6507e34b98d27c7f5754949a36e16
fronts1231 <takumi.maeomote1231@gmail.com>
2021-03-05 20:00:41 +0900

    post one item

--- main.rb
+++ main.rb
@@ -1,6 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'csv'
 
 get '/' do
-  'thanks'
+  @title = 'メモアプリ'
+  @subtitle = 'show saved notes here'
+  erb :index
+end
+
+get '/new' do
+  @title = 'メモアプリ'
+  @subtitle = 'create new note here'
+  @button = 'send'
+  erb :edit
+end
+
+get '/:name' do |n|
+  @title = 'メモアプリ'
+  @subtitle = "show #{n} content here"
+  erb :index
+end
+
+get '/:name/edit' do |n|
+  @title = 'メモアプリ'
+  @subtitle = "edit #{n} here"
+  @button = "edit"
+  erb :edit
+end
+
+post '/' do
+  @title = 'メモアプリ'
+  @subtitle = 'show saved notes here'
+  @titles = params[:title]
+  erb :index
 end
