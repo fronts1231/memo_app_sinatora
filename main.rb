4c35194c69b5d20b90ed3af236a1903dcabd4fd1
fronts1231 <takumi.maeomote1231@gmail.com>
2021-03-08 16:05:41 +0900

    consmetics

--- main.rb
+++ main.rb
@@ -5,14 +5,14 @@ $stdout.sync = true
 
 get '/' do
   @main_title = MAIN_TITLE
-  @subtitle = 'show saved notes here'
+  @subtitle = 'you can keep your idea here'
   @titles = CSV.read(DATA_FILE).map{|row| row[0]}
   erb :index
 end
 
 get '/new' do
   @main_title = MAIN_TITLE
-  @subtitle = 'create new note here'
+  @subtitle = 'fill the title and content in the form below'
   @title = ''
   @content = ''
   erb :add_item
@@ -20,6 +20,7 @@ end
 
 get '/:id' do |n|
   @main_title = MAIN_TITLE
+  @subtitle = 'please confirm the content of the selected item'
   data = CSV.read(DATA_FILE)[n.to_i]
   @title = data[0]
   @content = data[1]
@@ -40,7 +41,7 @@ end
 
 post '/new/confirm' do
   @main_title = MAIN_TITLE
-  @subtitle = 'show saved notes here'
+  @subtitle = 'new item created as below'
   
   if params[:title] != ''
     @title = params[:title]
@@ -49,7 +50,6 @@ post '/new/confirm' do
       data.puts([params[:title],params[:content]])
     data.close
     @titles = CSV.table(DATA_FILE).map{|row| row[0]}
-  
     erb :confirm
 
   else
@@ -62,10 +62,9 @@ end
 
 delete '/:id/deleted' do |n|
   @main_title = MAIN_TITLE
-  @subtitle = 'this item has been deleted'
+  @subtitle = 'this item has been successfully deleted'
   @title = ''
   @content = ''
-
   data = CSV.read(DATA_FILE)
   data.delete_at(n.to_i)
   CSV.open(DATA_FILE,'w') do |row|
@@ -73,7 +72,6 @@ delete '/:id/deleted' do |n|
       row.puts(item)
     end
   end
-  
   erb :confirm
 end
 
@@ -82,7 +80,7 @@ patch '/:id/edit/confirm' do |n|
   @index = n
   
   if params[:title] != ''
-    @subtitle = 'this item has been updated as below'
+    @subtitle = 'this item has been successfully updated as below'
     @title = params[:title]
     @content = params[:content]
     data = CSV.read(DATA_FILE)
