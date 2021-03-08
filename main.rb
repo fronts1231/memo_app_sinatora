4d15aeb8293053280e7bdfbfe7d3271bae4e718e
fronts1231 <takumi.maeomote1231@gmail.com>
2021-03-08 16:17:54 +0900

    consmetics

--- main.rb
+++ main.rb
@@ -5,22 +5,17 @@ $stdout.sync = true
 
 get '/' do
   @main_title = MAIN_TITLE
-  @subtitle = 'you can keep your idea here'
   @titles = CSV.read(DATA_FILE).map{|row| row[0]}
   erb :index
 end
 
 get '/new' do
   @main_title = MAIN_TITLE
-  @subtitle = 'fill the title and content in the form below'
-  @title = ''
-  @content = ''
   erb :add_item
 end
 
 get '/:id' do |n|
   @main_title = MAIN_TITLE
-  @subtitle = 'please confirm the content of the selected item'
   data = CSV.read(DATA_FILE)[n.to_i]
   @title = data[0]
   @content = data[1]
@@ -54,8 +49,6 @@ post '/new/confirm' do
 
   else
     @subtitle = 'title can not be blank'
-    @title = ''
-    @content = ''
     erb :confirm
   end
 end
@@ -63,8 +56,6 @@ end
 delete '/:id/deleted' do |n|
   @main_title = MAIN_TITLE
   @subtitle = 'this item has been successfully deleted'
-  @title = ''
-  @content = ''
   data = CSV.read(DATA_FILE)
   data.delete_at(n.to_i)
   CSV.open(DATA_FILE,'w') do |row|
@@ -94,8 +85,6 @@ patch '/:id/edit/confirm' do |n|
     
   else
     @subtitle = 'title can not be blank'
-    @title = ''
-    @content = ''
     erb :confirm
   end
 end
